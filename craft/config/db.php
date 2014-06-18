41eba18c84a6920673eb66ddfe46d274f23692c4
Helen V. Holmes <helen.v.holmes@gmail.com>
2014-06-17 23:46:37 -0400

    LAWL fuck this

--- craft/config/db.php
+++ craft/config/db.php
@@ -10,16 +10,16 @@
 return array(
 
 	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => 'localhost',
+	'server' => '127.0.0.1',
 
 	// The database username to connect with.
 	'user' => 'root',
 
 	// The database password to connect with.
-	'password' => '',
+	'password' => 'heybitch',
 
 	// The name of the database to select.
-	'database' => '',
+	'database' => 'wwcodedc_site',
 
 	// The prefix to use when naming tables. This can be no more than 5 characters.
 	'tablePrefix' => 'craft',
