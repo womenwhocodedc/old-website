fb25514b423f1b90cc099f10a44e1825074ca65a
Helen V. Holmes <helen.v.holmes@gmail.com>
2014-07-26 16:54:51 -0400

    Not really working, let's move on

--- gulp/tasks/watch.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('watch', ['setWatch', 'browserSync'], function() {
-	// gulp.watch('public/sass/**', ['sass']);
-	gulp.watch('./public/images/**', ['images']);
-	// Note: The browserify task handles js recompiling with watchify
-});
